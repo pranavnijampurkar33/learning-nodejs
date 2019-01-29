9af02774e4d6bcae169e83837a223d7f3412e9ff
Pranav Nijampurkar <Pranav.Nijampurkar@harbingergroup.com>
2019-01-29 15:43:57 +0530

    END MVC - Reusing the redundant code in Models. Starting with enhancements of current application. The next commits will be Synced with NodeJS Udemy course -> nodejs-the-complete-guide section 8/9

--- ExpressJS/models/product.js
+++ ExpressJS/models/product.js
@@ -2,37 +2,33 @@
 const path = require('path');
 const fs = require('fs');
 const rootDir = require('../utils/path');
+const p = path.join(rootDir,
+    'data',
+    'products.json'
+);
+const getProductFromFile = (cb) =>{
+    let products = [];
+    fs.readFile(p,(err,fileContent) => {
+        if (err){
+            return cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    });
+};
 module.exports = class Product {
     constructor(t){
         this.title = t;
     }
     save() {
-        const p = path.join(rootDir,
-            'data',
-            'products.json'
-        );
-        fs.readFile(p,(err,fileContent) => {
-            let products = [];
-            if(!err){
-                products = JSON.parse(fileContent);
-            }
+        getProductFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log('done');
+                console.log('Wrote successfully');
             });
         });
+
     }
     static fetchAll(cb) {
-        let products = [];
-        const p = path.join(rootDir,
-            'data',
-            'products.json'
-        );
-        fs.readFile(p,(err,fileContent) => {
-            if (err){
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
+        getProductFromFile(cb);
     }
 }
